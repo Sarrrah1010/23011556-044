c16b5f375b5f2ed149b2a136d6bc7879a66701bf
Sarrrah1010 <sarahbadar20005@gmail.com>
2025-10-05 13:34:57 +0500

    my commits

---
+++ CSharpPrograms/CSharpPrograms/EvenOrOddArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class EvenOdd
+{
+    public void EvenOrOddArray()
+    {
+        int[] numbers = new int[10];
+        int evenCount = 0, oddCount = 0;
+
+        Console.WriteLine("Enter 10 numbers:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Number i + 1: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+
+            if (numbers[i]%2 == 0){
+                evenCount++;
+            }
+            else
+                oddCount++;
+
+
+            Console.WriteLine("\nEven numbers: {evenCount}");
+            Console.WriteLine($"Odd numbers: {oddCount}");
+        }
+    }
+}
